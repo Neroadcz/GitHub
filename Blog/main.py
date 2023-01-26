846b9cf8e3d05bebb2c602967d6deccd9ceb0c7f
Neroad <mr.jiribily@gmail.com>
2023-01-26 19:20:54 +0100

    brackets

--- Blog/main.py
+++ Blog/main.py
@@ -14,7 +14,7 @@ import os
 # 8BYkEfBA6O6donzWlSihBXox7C0sKR6b
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get['SECRET_KEY']
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 
 
